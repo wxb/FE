bc54262caa26adfdd34af487e5a8680aa9fa0432
wangxb <wxb0328@gmail.com>
2016-03-23 15:22:48 +0800

    yeoman codelab

--- yeoman/angularApp/README.md
+++ yeoman/angularApp/README.md
@@ -10,3 +10,12 @@ Run `grunt` for building and `grunt serve` for preview.
 ## Testing
 
 Running `grunt test` will run the unit tests with karma.
+
+___
+
+## 碰到问题
+
+* `Cannot read property 'contents' of undefined` ？      
+解决：`"grunt-contrib-imagemin": "^1.0.0",` and `"imagemin":"^4.0.0"`
+
+* 同时也要时刻注意是否由于**权限**问题引起的 `grunt serve`, `grunt test`, `grunt`等任务执行错误
--- yeoman/angularApp/app/scripts/controllers/main.js
+++ yeoman/angularApp/app/scripts/controllers/main.js
@@ -7,6 +7,7 @@
  * # MainCtrl
  * Controller of the yeomanApp
  */
+ /*
 angular.module('yeomanApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
@@ -14,4 +15,27 @@ angular.module('yeomanApp')
       'AngularJS',
       'Karma'
     ];
+  });
+*/
+
+
+angular.module('yeomanApp')
+  .controller('MainCtrl', function ($scope, localStorageService) {
+      var todosInStore = localStorageService.get('todos');
+
+      $scope.todos = todosInStore || [];
+
+      $scope.$watch('todos', function () {
+        localStorageService.set('todos', $scope.todos);
+      }, true);
+
+      $scope.addTodo = function () {
+        $scope.todos.push($scope.todo);
+        $scope.todo = '';
+      };
+
+      $scope.removeTodo = function (index) {
+        $scope.todos.splice(index, 1);
+      };
+
   });
